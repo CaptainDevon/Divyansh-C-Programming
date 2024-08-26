8446b06cfa37011ab201cb3243b66f07b570e163
CaptainDevon <divyansh2004ind@gmail.com>
2024-08-26 22:55:47 +0530

    updations

--- Divyansh DSA/tree/tree_implementation.cpp
+++ Divyansh DSA/tree/tree_implementation.cpp
@@ -1,73 +1,153 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class treenode
 {
-    public:
+public:
     int data;
-    treenode *right;
-    treenode *left;
-
-    treenode(int value)
-    {
-        this->data = value;
-        this->left = nullptr;
-        this->right = nullptr;
-    }
-};
-class tree_implementation
-{
-private:
-    treenode *root;
+    treenode *l;
+    treenode *r;
 
 public:
-    void preorder_traversal(treenode *root)
+    treenode(int d)
     {
-        if (root == nullptr)
-            return;
-        cout << root->data << " ";
-        preorder_traversal(root->left);
-        preorder_traversal(root->right);
-    }
-
-    void inorder_traversal(treenode* root)
-    {
-        if(root==nullptr)return;
-        inorder_traversal(root->left);
-        cout<<root->data<<" ";
-        inorder_traversal(root->right);
-    }
-
-    void postorder_traversal(treenode* root)
-    {
-        if(root==nullptr)return;
-        postorder_traversal(root->left);
-        postorder_traversal(root->right);
-        cout<<root->data<<" ";
-    }
-
-    int countNodes(treenode* root)
-    {
-        if(root==nullptr)return 0;
-        return 1+countNodes(root->left)+countNodes(root->right);
+        data = d;
+        l = nullptr;
+        r = nullptr;
     }
 };
+
+void preorder_traversal(treenode *root)
+{
+    if (root == nullptr)
+        return;
+    cout << root->data << " ";
+    preorder_traversal(root->l);
+    preorder_traversal(root->r);
+}
+
+void inorder_traversal(treenode *root)
+{
+    if (root == nullptr)
+        return;
+    inorder_traversal(root->l);
+    cout << root->data << " ";
+    inorder_traversal(root->r);
+}
+
+void postorder_traversal(treenode *root)
+{
+    if (root == nullptr)
+        return;
+    postorder_traversal(root->l);
+    postorder_traversal(root->r);
+    cout << root->data << " ";
+}
+
+int level(treenode *root)
+{
+    if (root == nullptr)
+    {
+        return -1; // Height of an empty tree is -1
+    }
+
+    int leftHeight = level(root->l);
+    int rightHeight = level(root->r);
+
+    return 1 + max(leftHeight, rightHeight);
+}
+
+treenode *insertion(treenode *root, int value)
+{
+    if (root == nullptr)
+    {
+        return new treenode(value);
+    }
+
+    if (value < root->data)
+    {
+        root->l = insertion(root->l, value);
+    }
+    else if (value > root->data)
+    {
+        root->r = insertion(root->r, value);
+    }
+
+    return root;
+}
+treenode *findMin(treenode *node)
+{
+    while (node->l != nullptr)
+    {
+        node = node->l;
+    }
+    return node;
+}
+
+treenode *deletion(treenode *root, int value)
+{
+    if (root == nullptr)
+        return root;
+    if (value < root->data)
+        root->l = deletion(root->l, value);
+    else if (value > root->data)
+        root->r = deletion(root->r, value);
+    else
+    {
+        if (root->l == nullptr)
+        {
+            treenode *temp = root->r;
+            delete root;
+            return temp;
+        }
+        else if (root->r == nullptr)
+        {
+            treenode *temp = root->l;
+            delete root;
+            return temp;
+        }
+        treenode *temp = findMin(root->r);
+        root->data = temp->data;
+        root->r = deletion(root->r, temp->data);
+    }
+    return root;
+}
 
 int main()
 {
-    treenode* root=new treenode(0);
-    root->left=new treenode(1);
-    root->right=new treenode(2);
-    root->left->left=new treenode(3);
-    root->left->right=new treenode(4);
-    root->right->left=new treenode(5);
-    root->right->right=new treenode(6);
-    tree_implementation t;
-    cout<<t.countNodes(root)<<endl;
-    t.preorder_traversal(root);
-    cout<<endl;
-    t.inorder_traversal(root);
-    cout<<endl;
-    t.postorder_traversal(root);
+    treenode *root = new treenode(0);
+    treenode *root1;
+    root->l = new treenode(1);
+    root->r = new treenode(2);
+    root->l->l = new treenode(3);
+    root->l->r = new treenode(4);
+    root->r->l = new treenode(5);
+    root->r->r = new treenode(6);
+    root->l->l->l = new treenode(8);
+    root->l->l->r = new treenode(9);
+    root->l->r->l = new treenode(10);
+
+    preorder_traversal(root);
+    cout << endl;
+    inorder_traversal(root);
+    cout << endl;
+    postorder_traversal(root);
+    cout << endl;
+    cout << level(root) << endl;
+    cout << "----------------------------------------------------------";
+    root1 = insertion(root1, 1);
+    root1 = insertion(root1, 5);
+    root1 = insertion(root1, 2);
+    root1 = insertion(root1, 3);
+    root1 = insertion(root1, 4);
+    root1 = insertion(root1, 6);
+    cout << endl;
+    postorder_traversal(root1);
+    cout << endl;
+    cout << "----------------------------------------------------------";
+    cout << endl;
+    root1 = deletion(root1, 3);
+    root1 = deletion(root1, 6);
+    postorder_traversal(root1);
+
     return 0;
 }
