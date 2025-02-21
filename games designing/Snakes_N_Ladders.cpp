e19ed4b1fdb71bbda59f6d48b9b21d1d64b86a1b
CaptainDevon <divyansh2004ind@gmail.com>
2025-02-21 18:05:16 +0530

    snake game modifications

--- games designing/Snakes_N_Ladders.cpp
+++ games designing/Snakes_N_Ladders.cpp
@@ -1,80 +1,82 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Board
 {
-    protected:
-    vector<pair<int,int>> snakes={{16,6},{48,26},{49,16},{64,60},{87,24},{93,73},{98,78}};
-    vector<pair<int,int>> ladders={{4,14},{9,31},{21,42},{28,84},{51,67},{71,91},{80,97}};
+protected:
+    vector<pair<int, int>> snakes = {{16, 6}, {48, 26}, {49, 16}, {64, 60}, {87, 24}, {93, 73}, {98, 78}};
+    vector<pair<int, int>> ladders = {{4, 14}, {9, 31}, {21, 42}, {28, 84}, {51, 67}, {71, 91}, {80, 97}};
 };
 
-class Game:public Board
+class Game : public Board
 {
-    private:
-    vector<int>players;
+private:
+    vector<int> players;
 
-    int getRandomNumber() 
+    int getRandomNumber()
     {
         return (rand() % 6) + 1;
     }
 
     int snakeBite(int tile)
     {
-        for(int i=0;i<snakes.size();i++)
+        for (int i = 0; i < snakes.size(); i++)
         {
-            if(snakes[i].first==tile)return i;
+            if (snakes[i].first == tile)
+                return i;
         }
         return -1;
     }
 
     int ladderClimb(int tile)
     {
-        for(int i=0;i<ladders.size();i++)
+        for (int i = 0; i < ladders.size(); i++)
         {
-            if(ladders[i].first==tile) return i;
+            if (ladders[i].first == tile)
+                return i;
         }
         return -1;
     }
 
     void scoreDisplay()
     {
-        for(auto it:players)
+        for (auto it : players)
         {
-            cout<<it<<" ";
+            cout << it << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
     void playTurn(int i)
     {
-        int number=getRandomNumber();
-        if(this->players[i]<=100)this->players[i]+=number;
-        int snakeIndex=snakeBite(this->players[i]);
-        int ladderIndex=ladderClimb(this->players[i]);
-        if(snakeIndex!=-1)
+        int number = getRandomNumber();
+        if (this->players[i] + number <= 100)
+            this->players[i] += number;
+        int snakeIndex = snakeBite(this->players[i]);
+        int ladderIndex = ladderClimb(this->players[i]);
+        if (snakeIndex != -1)
         {
-            cout<<"Snake bite!! phisssssss..."<<endl;
-            this->players[i]=snakes[snakeIndex].second;
+            cout << "Snake bite!! phisssssss..." << endl;
+            this->players[i] = snakes[snakeIndex].second;
         }
-        if(ladderIndex!=-1)
+        if (ladderIndex != -1)
         {
-            cout<<"Wao!! you found a ladder..."<<endl;
-            this->players[i]=ladders[ladderIndex].second;
+            cout << "Wao!! you found a ladder..." << endl;
+            this->players[i] = ladders[ladderIndex].second;
         }
-        
-        
     }
 
     bool winCondition(int tile)
     {
-        if(tile==100) return true;
+        if (tile == 100)
+            return true;
         return false;
     }
 
-    public:
+public:
     Game(int players)
     {
-        this->players=vector<int>(players,0);
+        this->players = vector<int>(players, 0);
     }
 
     void gameInformation()
@@ -92,21 +94,22 @@ class Game:public Board
 
     void playground()
     {
-        while(true)
+        while (true)
         {
-            int stopper=0;
-            for(int i=0;i<players.size();i++)
+            int stopper = 0;
+            for (int i = 0; i < players.size(); i++)
             {
                 playTurn(i);
-                if(winCondition(players[i]))
+                if (winCondition(players[i]))
                 {
-                    stopper+=1;
-                    cout<<"player number "<<i+1<<" is the Winner!! congratulations..."<<endl;
+                    stopper += 1;
+                    cout << "player number " << i + 1 << " is the Winner!! congratulations..." << endl;
                     break;
                 }
-                scoreDisplay();
             }
-            if(stopper==1)break;
+            scoreDisplay();
+            if (stopper == 1)
+                break;
         }
     }
 };
@@ -114,8 +117,8 @@ class Game:public Board
 int main()
 {
     int players;
-    cout<<"enter number of players: ";
-    cin>>players;
+    cout << "enter number of players: ";
+    cin >> players;
     Game g(players);
     g.gameInformation();
     g.playground();
